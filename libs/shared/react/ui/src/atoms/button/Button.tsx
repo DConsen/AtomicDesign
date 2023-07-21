24ade2754d736a77dc985c41e16e6810c64b2a28
Diego Consen <diego.consen@navara.nl>
2023-07-21 16:38:46 +0200

    fix(shared-react-ui): format code

--- libs/shared/react/ui/src/atoms/button/Button.tsx
+++ libs/shared/react/ui/src/atoms/button/Button.tsx
@@ -2,6 +2,7 @@ export interface ButtonProps {
     label: string;
 }
 
+// Trigger build
 export function Button({ label }: ButtonProps) {
     return <button className="p-4 bg-blue-500">{label}</button>;
 }
