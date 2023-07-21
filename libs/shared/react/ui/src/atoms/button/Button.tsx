1bdffa42aeb8c74c0b363afc0174be2c97019385
Diego Consen <diego.consen@navara.nl>
2023-07-21 16:24:27 +0200

    feat(shared-react-ui): add chromatic to ci

--- libs/shared/react/ui/src/atoms/button/Button.tsx
+++ libs/shared/react/ui/src/atoms/button/Button.tsx
@@ -1,9 +1,9 @@
 export interface ButtonProps {
-  label: string;
+    label: string;
 }
 
 export function Button({ label }: ButtonProps) {
-  return <button className="p-4 bg-red-500">{label}</button>;
+    return <button className="p-4 bg-blue-500">{label}</button>;
 }
 
 export default Button;
