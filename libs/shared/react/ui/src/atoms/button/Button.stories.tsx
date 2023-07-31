aea9f34bc341892666cb5004541c0845f73b24ec
Diego Consen <diego.consen@navara.nl>
2023-07-31 09:44:47 +0200

    chore(shared-react-ui): format files

--- libs/shared/react/ui/src/atoms/button/Button.stories.tsx
+++ libs/shared/react/ui/src/atoms/button/Button.stories.tsx
@@ -1,5 +1,5 @@
-import type {Meta} from '@storybook/react';
-import {Button} from './Button';
+import type { Meta } from '@storybook/react';
+import { Button } from './Button';
 
 const Story: Meta<typeof Button> = {
     component: Button,
@@ -11,10 +11,10 @@ export const Primary = {
     args: {
         label: 'Lorem Ipsum',
     },
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/7Ncum8MGXNW6VFCsTR59qr/Atomic-Design?type=design&node-id=54%3A168&mode=design&t=8DNrpMOgdwSIhheu-1'
-    }
-  }
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/7Ncum8MGXNW6VFCsTR59qr/Atomic-Design?type=design&node-id=54%3A168&mode=design&t=8DNrpMOgdwSIhheu-1',
+        },
+    },
 };
