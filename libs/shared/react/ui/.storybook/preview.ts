040247e95e70d6014f8b65ef39c1bad9f79682b5
Diego Consen <diego.consen@navara.nl>
2023-08-01 13:21:09 +0200

    feat(shared-react-ui): add extra tailwind config

--- libs/shared/react/ui/.storybook/preview.ts
+++ libs/shared/react/ui/.storybook/preview.ts
@@ -1,1 +1,64 @@
 import './index.css';
+
+export const parameters = {
+    viewport: {
+        viewports: {
+            desktop_1: {
+                name: 'Desktop #1',
+                styles: {
+                    width: '1920px',
+                    height: '1080px',
+                },
+                type: 'desktop',
+            },
+            desktop_2: {
+                name: 'Desktop #2',
+                styles: {
+                    width: '1366px',
+                    height: '768px',
+                },
+                type: 'desktop',
+            },
+            desktop_3: {
+                name: 'Desktop #3',
+                styles: {
+                    width: '1536px',
+                    height: '864px',
+                },
+                type: 'desktop',
+            },
+            mobile_large: {
+                name: 'Mobile (large)',
+                styles: {
+                    width: '390px',
+                    height: '844px',
+                },
+                type: 'mobile',
+            },
+            mobile_small: {
+                name: 'Mobile (small)',
+                styles: {
+                    width: '375px',
+                    height: '667px',
+                },
+                type: 'mobile',
+            },
+            tablet: {
+                name: 'Tablet (portait)',
+                styles: {
+                    width: '768px',
+                    height: '1024px',
+                },
+                type: 'tablet',
+            },
+            tablet_landscape: {
+                name: 'Tablet (landscape)',
+                styles: {
+                    width: '1024px',
+                    height: '768px',
+                },
+                type: 'tablet',
+            },
+        },
+    },
+};
--- libs/shared/react/ui/src/components/atoms/Button/Button.tsx
+++ libs/shared/react/ui/src/components/atoms/Button/Button.tsx
@@ -2,9 +2,8 @@ export interface ButtonProps {
     label: string;
 }
 
-// Trigger build
 export function Button({ label }: ButtonProps) {
-    return <button className="p-4 bg-secondary rounded-full font-display font-bold">{label}</button>;
+    return <button className="p-4 bg-secondary dark:bg-primary rounded-full font-display font-bold">{label}</button>;
 }
 
 export default Button;
