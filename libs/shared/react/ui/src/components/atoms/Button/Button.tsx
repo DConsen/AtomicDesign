33201dca65e593649040cebea9f8b40ffd82a5d3
Diego Consen <diego.consen@navara.nl>
2023-08-03 13:39:02 +0200

    chore(shared-react-ui): change button atom

--- libs/shared/react/ui/src/components/atoms/Button/Button.tsx
+++ libs/shared/react/ui/src/components/atoms/Button/Button.tsx
@@ -3,7 +3,9 @@ export interface ButtonProps {
 }
 
 export function Button({ label }: ButtonProps) {
-  return <button className="p-4 bg-secondary dark:bg-primary rounded-full font-display font-bold">{label}</button>;
+  return (
+    <button className="py-4 px-8 bg-secondary dark:bg-primary rounded-full font-display font-bold">{label}</button>
+  );
 }
 
 export default Button;
